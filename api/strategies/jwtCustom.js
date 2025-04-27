f755073ab3ae07c27d48a53b60ecfdaca19486f9
Dimitar Slaev <dimitar.slaev@infomaniak.com>
2025-04-27 16:58:34 +0200

    cache dynamic import

--- api/strategies/jwtCustom.js
+++ api/strategies/jwtCustom.js
@@ -4,20 +4,25 @@ const { Strategy, ExtractJwt } = require('passport-jwt');
 const signPayload = require('~/server/services/signPayload');
 const { logger } = require('~/config');
 
-module.exports = () => {
-  try {
-    return require(process.env.JWT_CUSTOM_STRATEGY_PATH)({
-      findUser,
-      createUser,
-      updateUser,
-      SystemRoles,
-      Strategy,
-      ExtractJwt,
-      signPayload,
-      logger,
-    });
-  } catch (error) {
-    logger.error(`Failed to load strategy: ${error.message}`);
-  }
-};
+// Since we're using a dynamic import, Node.js does not cache the required module automatically
+let cachedModule = null;
 
+module.exports = () => {
+  if (!cachedModule) {
+    try {
+      cachedModule = require(process.env.JWT_CUSTOM_STRATEGY_PATH)({
+        findUser,
+        createUser,
+        updateUser,
+        SystemRoles,
+        Strategy,
+        ExtractJwt,
+        signPayload,
+        logger,
+      });
+    } catch (error) {
+      logger.error(`Failed to load strategy: ${error.message}`);
+    }
+  }
+  return cachedModule;
+};
